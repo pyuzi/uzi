dc979a0bd235c8f1d9fbd0c17095d07f375f0b1d
David Kyalo <davidmkyalo@gmail.com>
2022-04-30 01:21:33 +0300

    inital pro implementation

---
+++ xdi/exceptions.py
@@ -0,0 +1,45 @@
+
+
+import attr
+
+
+class XdiException(Exception):
+    """Base class for all internal exceptions."""
+
+
+
+@attr.s()
+class FinalProviderOverrideError(TypeError, XdiException):
+    """Raised when a final provider has an override.
+    """
+    
+    abstract: 'Injectable' = attr.ib(default=None)
+    final: 'Provider' = attr.ib(default=None)
+    overrides: tuple['Provider' ]= attr.ib(default=(), converter=tuple)
+
+
+
+@attr.s()
+class InjectorLookupError(KeyError, XdiException):
+    """Raised by ~Injector` when a missing dependency is requested.
+    
+    Args:
+        abstract (Injectable): the missing dependency
+    """
+
+    abstract: "Injectable" = attr.ib(default=None)
+    scope: "Scope" = attr.ib(default=None)
+
+
+
+
+class ProIndexError(IndexError, XdiException):
+    """Raised 
+    """
+
+
+
+
+from .providers import Provider
+from .core import Injectable
+from .scopes import Scope
